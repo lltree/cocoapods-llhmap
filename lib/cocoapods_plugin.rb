5ff9ca0e5788b85ab40841a11a9c5872d44f65eb
gaochunlei <gaochunlei@58.com>
2022-04-21 19:17:59 +0800

    测试

--- lib/cocoapods_plugin.rb
+++ lib/cocoapods_plugin.rb
@@ -1,1 +1,20 @@
 require 'cocoapods-llhmap/command'
+
+module CocoapodsMyplugin
+    class PrinterTest
+      def printHello
+        puts "this is myplugin action"
+      end
+    end
+end
+
+
+module LLPodHeaderMap
+    #注册post_install钩子
+    HooksManager.register('cocoapods-llhmap', :post_install) do |installer|
+        CocoapodsMyplugin.PrinterTest.new.printHello()
+    end
+
+
+
+end
