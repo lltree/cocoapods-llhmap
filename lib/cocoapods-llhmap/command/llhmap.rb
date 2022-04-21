2b5df23f29c677fd9855068d662d167655bc6ee1
gaochunlei <gaochunlei@58.com>
2022-04-21 19:23:20 +0800

    Revert "fix"
    This reverts commit f2d8c71f9a4cfea56bb8c6f4f4befb615598b23e.

--- lib/cocoapods-llhmap/command/llhmap.rb
+++ lib/cocoapods-llhmap/command/llhmap.rb
@@ -17,7 +17,7 @@ module Pod
     # @todo Create a PR to add your plugin to CocoaPods/cocoapods.org
     #       in the `plugins.json` file, once your plugin is released.
     #
-    class llhmap < Command
+    class Llhmap < Command
       self.summary = 'Short description of cocoapods-llhmap.'
 
       self.description = <<-DESC
